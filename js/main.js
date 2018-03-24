818e3f4aa63e7b1b4c6ecfa3e280ce5922675ba6
Ayush Pandey <intern.ayush.pandey@redcarpetup.com>
2018-03-24 20:44:22 +0530

    test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("mar 24, 2018 20:42:00").getTime();
+var countDownDate = new Date("mar 24, 2018 20:50:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
