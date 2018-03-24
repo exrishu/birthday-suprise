15dc963df6d8e7e75a4ae862f3fa1ed0ce4c17a7
Ayush Pandey <intern.ayush.pandey@redcarpetup.com>
2018-03-24 22:31:18 +0530

    timming-updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("mar 25, 2018 00:00:00").getTime();
+var countDownDate = new Date("mar 24, 2018 11:55:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
